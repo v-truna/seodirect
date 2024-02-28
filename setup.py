cef2acce744da3658437b9cde901a023adb23dc3
Dimas Novandra Utomo <156698145+v-truna@users.noreply.github.com>
2024-02-28 16:47:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from collections import OrderedDict
+from setuptools import setup
+
+
+setup(
+    name='seo',
+    version='0.0.5dev',
+    url='https://github.com/ceb10n/seo',
+    project_urls=OrderedDict((
+        ('Code', 'https://github.com/ceb10n/seo'),
+    )),
+    author='Rafael Marques',
+    author_email='rafaelomarques@gmail.com',
+    maintainer='Rafael Marques',
+    maintainer_email='rafaelomarques@gmail.com',
+    description='Simple collection of SEO utils.',
+    long_description=open('README.md').read(),
+    packages=['seo'],
+    license='MIT License',
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'fake-useragent>=0.1.11',
+    ],
+)
